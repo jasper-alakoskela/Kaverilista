f6dddc759e9ce418228cd4caf44fca21986956ee
Jasper Alakoskela <jasper.alakosekla@gmail.com>
2020-08-21 13:58:31 +0300

    melkein valmis toinen puoli mutta pari ongelmaa

--- Code/kaverilista.js
+++ Code/kaverilista.js
@@ -1,23 +1,20 @@
-
 const kaverilista = document.getElementById("kaverilista");
 
-
-
 /* prompt loop */
+
 let nameLoop = 0;
 let names = [];
 
-
 function namePrompt() {
-    
-    
-    while(nameLoop < 10){
-        names.push(prompt("Kerro kymmenen kaverin nimet"));
-        nameLoop++;
-    }
-
-    //for loopilla nimet consoliin!
-
+  while (nameLoop < 10) {
+    names.push(prompt("Kerro kymmenen kaverin nimet"));
+    nameLoop++;
+  }
+  for (let i = 0; i < names.length; i++) {
+    console.log(names[i]); // tässä on ongelma miten saada consoleen ilman logia???
+  }
+  const newLi = document.createElement("LI");
+  const liContent = document.createTextNode(.value); //Tässä on ongelma
+  newLi.appendChild(liContent);
+  kaverilista.appendChild(newLi);
 }
-
-
