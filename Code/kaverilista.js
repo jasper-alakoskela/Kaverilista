fd7575ba4d4bb902d893d9ecfdfa07ec13e4194b
Jasper Alakoskela <jasper.alakoskela@itvelhot.local>
2020-08-25 13:47:37 +0300

    molemmat html styletty

--- Code/kaverilista.js
+++ Code/kaverilista.js
@@ -1,23 +1,40 @@
+/*Versio 1*/
+const kaveriLista = document.getElementById("kaverilista");
 
-const kaverilista = document.getElementById("kaverilista");
-
-
-
-/* prompt loop */
 let nameLoop = 0;
 let names = [];
 
-
 function namePrompt() {
-    
-    
-    while(nameLoop < 10){
-        names.push(prompt("Kerro kymmenen kaverin nimet"));
-        nameLoop++;
-    }
+  while (nameLoop < 10) {
+    names.push(prompt("Kerro kymmenen kaverin nimet"));
+    nameLoop++;
+  }
 
-    //for loopilla nimet consoliin!
+  for (let i = 0; i < names.length; i++) {
+    console.log(names[i]);
 
+    const newLi = document.createElement("LI");
+    const liContent = document.createTextNode(names[i]);
+    newLi.appendChild(liContent);
+    kaveriLista.appendChild(newLi);
+  }
 }
+
+/*Versio 2*/
+const kaveriLista2 = document.getElementById("kaverilista2");
+const listInput = document.getElementById("list-input");
+const addBtn = document.getElementById("add");
+const removeBtn = document.getElementById("remove");
+
+
+/*Lisää kaveri*/
+addBtn.addEventListener("click", function() {
+  const newLi = document.createElement("LI");
+  const liContent = document.createTextNode(listInput.value);
+  newLi.appendChild(liContent);
+  kaveriLista2.appendChild(newLi);
+});
+
+/*Poista kaveri*/
 
 
